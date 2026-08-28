0a161149886c04b22fe4865f71115c5a41a3fc9e
Syd Amir <hemedani@gmail.com>
2026-08-28 20:38:02 +0330

    :sparkles: feat(front): Add announcement action wrappers

---
+++ front/src/app/actions/announcement/add.ts
@@ -0,0 +1,10 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { AppApi } from "@/services/api";
+import type { ReqType } from "@/types/declarations/selectInp";
+
+export async function addAnnouncement(request: ReqType["main"]["announcement"]["add"]) {
+  const token = (await cookies()).get("token");
+  return AppApi().send({ service: "main", model: "announcement", act: "add", details: request }, { token: token?.value });
+}
